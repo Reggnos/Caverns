420b80527fbdfd71ca202f4f18a27a36d35ef039
Mateja Stankovic <stankovicmateja@yahoo.com>
2017-07-27 16:58:27 +0200

    Light setup

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -15,21 +15,26 @@ public class PlayerMove : MonoBehaviour {
 	
 	void Update ()
     {
+        //Debug.Log(gameObject.transform.localEulerAngles.z);
 		if (Input.GetKey(KeyCode.A))
         {
             rd.AddForce(Vector2.left * force);
-            //rd.velocity = Vector2.left * force;
         }
         else if (Input.GetKey(KeyCode.D))
         {
             rd.AddForce(Vector2.right * force);
-            //rd.velocity = Vector2.right * force;
         }
-      /*  
-        if ((Input.GetKeyDown(KeyCode.D)) || (Input.GetKeyDown(KeyCode.A)))
+        
+        /*
+        if (gameObject.transform.localEulerAngles.z > 180 & gameObject.transform.localEulerAngles.z < 330)
         {
-            rd.velocity = new Vector2(0, 0);
+            Debug.Log("izemdju minus 30 i 180");
+            transform.Rotate(0, 0, 1);
         }
-      */
+        else if (gameObject.transform.localEulerAngles.z > 30 && gameObject.transform.localEulerAngles.z < 180)
+        {
+            transform.Rotate(0, 0, -1);
+        }
+        */
     }
 }
